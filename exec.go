dd6755449074a54fabe5ea75033491bb6a7f4742
antmoveh <shen.ren.love@163.com>
2020-08-29 09:02:57 +0800

    docker exec

---
+++ exec.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"mydocker/container"
+	_ "mydocker/nsenter"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+const ENV_EXEC_PID = "mydocker_pid"
+const ENV_EXEC_CMD = "mydocker_cmd"
+
+func ExecContainer(containerName string, comArray []string) {
+	pid, err := getContainerPidByName(containerName)
+	if err != nil {
+		logrus.Errorf("Exec container getContainerPidByName %s error %v", containerName, err)
+		return
+	}
+	cmdStr := strings.Join(comArray, " ")
+	logrus.Infof("container pid %s", pid)
+	logrus.Infof("command %s", cmdStr)
+
+	cmd := exec.Command("/proc/self/exe", "exec")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	os.Setenv(ENV_EXEC_PID, pid)
+	os.Setenv(ENV_EXEC_CMD, cmdStr)
+
+	if err := cmd.Run(); err != nil {
+		logrus.Errorf("Exec container %s error %v", containerName, err)
+	}
+}
+
+func getContainerPidByName(containerName string) (string, error) {
+	dirURL := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	configFilePath := dirURL + container.ConfigName
+	contentBytes, err := ioutil.ReadFile(configFilePath)
+	if err != nil {
+		return "", err
+	}
+	var containerInfo container.ContainerInfo
+	if err := json.Unmarshal(contentBytes, &containerInfo); err != nil {
+		return "", err
+	}
+	return containerInfo.Pid, nil
+}
